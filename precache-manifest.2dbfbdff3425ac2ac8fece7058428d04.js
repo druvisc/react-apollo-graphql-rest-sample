b7d8f65b09c19f1a19f0a3aab022daa8e67509c2
Druvis Cukurs <druvis.cukurs@gmail.com>
2020-01-09 21:03:12 +0200

    Updates

---
+++ precache-manifest.2dbfbdff3425ac2ac8fece7058428d04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1762ea81122c0c44551541ce98788b89",
+    "url": "/react-apollo-graphql-rest-sample/index.html"
+  },
+  {
+    "revision": "9250d5cc319042e1270f",
+    "url": "/react-apollo-graphql-rest-sample/static/js/2.736a7647.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/react-apollo-graphql-rest-sample/static/js/2.736a7647.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10e1b3a06b8c7ce52538",
+    "url": "/react-apollo-graphql-rest-sample/static/js/main.b62f05be.chunk.js"
+  },
+  {
+    "revision": "405ea771e27f4c103fc8",
+    "url": "/react-apollo-graphql-rest-sample/static/js/runtime-main.569744c6.js"
+  }
+]);
